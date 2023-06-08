c8a389cac3a10956c31a3cf61aba6a4fa3b34a69
José Carlos Lara Gómez <LaraGomezJC@gmail.com>
2023-06-08 22:07:42 +0200

    Fix test failing execution due to port already on use by Kubernetes instance

--- src/test/java/com/josehawk/anagramtest/acceptance/configuration/SpringIntegrationConfiguration.java
+++ src/test/java/com/josehawk/anagramtest/acceptance/configuration/SpringIntegrationConfiguration.java
@@ -4,11 +4,16 @@ import com.josehawk.anagramtest.AnagramTestApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(
         classes = AnagramTestApplication.class,
         webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @CucumberContextConfiguration
 @ActiveProfiles("acceptance")
+@TestPropertySource(properties = {
+        "DICTIONARY_URL=https://api.dictionaryapi.dev/api/v2/entries/en",
+        "server.port=8091"
+})
 public class SpringIntegrationConfiguration {
 }
